1cc346e3f9703b86ec8e380968a575c064727da7
Sergey Baruzdin <irbis191@gmail.com>
2025-02-14 14:42:21 +0900

    Add Accounts table

--- account/models.py
+++ account/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Account(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    frist_name = models.CharField(max_length=200)
+    middle_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)    
+    date_of_birth = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.frist_name} {self.last_name} ({self.user.username})'
